2652cbc329c3a5917109d93e5b93ed0f8ee0eb75
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2018-04-22 16:25:16 +0700

    Update uF12_lihatStatistik.pas

--- Daftar Program/uF12_lihatStatistik.pas
+++ Daftar Program/uF12_lihatStatistik.pas
@@ -13,16 +13,16 @@ implementation
 	procedure mainLihatStatistik(ID : integer;  var dataSimulasi : tabelSimulasi); 
 	{ Menampilkan data simulasi }
 	begin
-		writeln('Jumlah Hari Hidup 			: ', dataSimulasi.itemKe[ID].jumlahHariHidup);
-		writeln('Jumlah Energi Saat Ini 	: ', dataSimulasi.itemKe[ID].jumlahEnergi);
+		writeln('Jumlah Hari Hidup          : ', dataSimulasi.itemKe[ID].jumlahHariHidup);
+		writeln('Jumlah Energi Saat Ini     : ', dataSimulasi.itemKe[ID].jumlahEnergi);
 		writeln('Kapasitas Maximum Inventori: ', dataSimulasi.itemKe[ID].kapasitasMaxInventori);
-		writeln('Total Bahan Mentah Dibeli	: ', dataSimulasi.itemKe[ID].totalBahanMentahDibeli);
-		writeln('Total Bahan Olahan Dibuat	: ', dataSimulasi.itemKe[ID].totalBahanOlahanDibuat);
-		writeln('Total Bahan OLahan Dijual	: ', dataSimulasi.itemKe[ID].totalBahanOlahanDijual);
-		writeln('Total Resep Dijual			: ', dataSimulasi.itemKe[ID].totalResepDijual);
-		writeln('Total Pemasukan			: ', dataSimulasi.itemKe[ID].totalPemasukan);
-		writeln('Total Pengeluaran			: ', dataSimulasi.itemKe[ID].totalPengeluaran);
-		writeln('Total Uang					: ', dataSimulasi.itemKe[ID].jumlahDuit);
+		writeln('Total Bahan Mentah Dibeli  : ', dataSimulasi.itemKe[ID].totalBahanMentahDibeli);
+		writeln('Total Bahan Olahan Dibuat  : ', dataSimulasi.itemKe[ID].totalBahanOlahanDibuat);
+		writeln('Total Bahan OLahan Dijual  : ', dataSimulasi.itemKe[ID].totalBahanOlahanDijual);
+		writeln('Total Resep Dijual         : ', dataSimulasi.itemKe[ID].totalResepDijual);
+		writeln('Total Pemasukan            : ', dataSimulasi.itemKe[ID].totalPemasukan);
+		writeln('Total Pengeluaran          : ', dataSimulasi.itemKe[ID].totalPengeluaran);
+		writeln('Total Uang                 : ', dataSimulasi.itemKe[ID].jumlahDuit);
 	end;
 
 end.
